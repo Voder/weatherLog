5a2d7180f3342805f9fb757d54c9ee0e7355fd79
Volker Aufschild <mail@volker-aufschild.de>
2014-05-29 14:36:46 +0200

    added simple timestamp logging

--- weatherlog.py
+++ weatherlog.py
@@ -9,6 +9,7 @@ class WeatherLogger(object):
         self.logger = serial.Serial('/dev/ttyUSB0', baudrate=19200, bytesize=8, stopbits=1, timeout=0 )
         self.path = path
     
+        
     def readData(self, maxtime=180):
         t = maxtime
         while t > 0:
@@ -24,19 +25,26 @@ class WeatherLogger(object):
         
             
     def writeData(self, data):
-        f = open(self.path,'w')
-        f.write(data)
-        f.write("\n")
-        f.close()
-    
+        with open(self.path,'a') as f:
+            f.write(currentTime() + "\n")
+            f.write(data)
+            f.write("\n")
 
+  
+def currentTime():
+    ts=time.time()
+    crt = time.strftime("%Y-%m-%d %H:%M:%S.",time.localtime(ts))
+    return str(crt)
+        
 def main():
     l = WeatherLogger()
     max = 100
+    print (currentTime())
     print ('Start Logging')
     while (max > 0):
         max = max-1
         ret = l.readData()
+        print (currentTime())
         if ret == 0:
             print ('No data received')
         else:
