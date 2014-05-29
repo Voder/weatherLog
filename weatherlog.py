4eed8d42886474acfb68725494227235274bed36
Volker Aufschild <mail@volker-aufschild.de>
2014-05-29 15:57:02 +0200

    log receiver info on program start and terminate if info cant be read

--- weatherlog.py
+++ weatherlog.py
@@ -10,25 +10,40 @@ class WeatherLogger(object):
         self.path = path
     
         
-    def readData(self, maxtime=180):
+    def readData(self):
+        b = self.logger.inWaiting()
+        if b > 0:
+            return self.logger.read(b)
+        return ''
+        
+    def readDataToFile(self, maxtime=180):
         t = maxtime
         while t > 0:
             t = t-1
             b = self.logger.inWaiting()
             if b > 0:
                 data = self.logger.read(b)
-                self.writeData(data)
+                self.writeDataToFile(data)
                 return 1
             time.sleep(1)
         return 0
         
-        
             
-    def writeData(self, data):
+    def writeDataToFile(self, data):
         with open(self.path,'a') as f:
             f.write(currentTime() + "\n")
             f.write(data)
-            f.write("\n")
+    
+    def logInfo(self):
+        self.logger.write("?")
+        time.sleep(1)
+        ret = self.readData()
+        if (len(ret) == 0):
+            print 'ERROR: Logger info could not be read!'
+            return -1
+        else:
+            print ret
+            return 1
 
   
 def currentTime():
@@ -38,12 +53,16 @@ def currentTime():
         
 def main():
     l = WeatherLogger()
+    i = l.logInfo()
+    if i < 0:
+        return 0
+        
     max = 100
     print (currentTime())
     print ('Start Logging')
     while (max > 0):
         max = max-1
-        ret = l.readData()
+        ret = l.readDataToFile()
         print (currentTime())
         if ret == 0:
             print ('No data received')
